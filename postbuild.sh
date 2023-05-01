490d155bdda5da41484a3b21eb9194cc4a9f047c
gian <nnkogift@hotmail.com>
2023-05-01 14:15:12 +0300

    feat(docker): Setup
    - Setup docker implementation of build
    
    Signed-off-by: gian <nnkogift@hotmail.com>

--- postbuild.sh
+++ postbuild.sh
@@ -5,6 +5,6 @@ PKG_VERSION=`node -p "require('./package.json').version"`
 
 echo "Building app"
 BUNDLE_NAME="hisptz-chatbot-$PKG_VERSION.zip"
-bestzip "$BUNDLE_NAME" app package.json .env.example prisma
+bestzip "$BUNDLE_NAME" app package.json .env.example yarn.lock prisma
 mkdir "build"
 mv $BUNDLE_NAME build
--- src/client/index.ts
+++ src/client/index.ts
@@ -8,14 +8,21 @@ export default prisma;
 
 export async function initPrisma() {
     return new Promise((resolve, reject) => {
-        console.info("Performing prisma changes")
-        const process = ChildProcess.exec(`npx prisma migrate deploy`)
-
-        process.on("exit", () => {
+        const process = ChildProcess.exec(`npx prisma migrate deploy`, {}, (error, stdout, stderr) => {
+            console.info(stdout);
+            console.info(error);
+            console.info(stderr);
+        })
+        process.on("message", console.info)
+        console.info(`Performing prisma changes`)
+        process.on("exit", (code, signal) => {
+            console.info(signal)
+            console.info(code)
             console.info("Done");
             resolve(0);
         })
         process.on("error", (error) => {
+            console.error(error)
             reject(error)
         })
     })
