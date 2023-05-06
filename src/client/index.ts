046704ae14ce4a4726dfee2db2cbb8429d4bc2f0
gian <nnkogift@hotmail.com>
2023-05-06 23:11:58 +0300

    fix(log): Fixes
    - Fixed log issues
    
    Signed-off-by: gian <nnkogift@hotmail.com>

--- src/client/index.ts
+++ src/client/index.ts
@@ -8,18 +8,11 @@ export default prisma;
 
 export async function initPrisma() {
     return new Promise((resolve, reject) => {
-        const process = ChildProcess.exec(`npx prisma migrate deploy`, {}, (error, stdout, stderr) => {
-            console.info(stdout);
-            console.info(error);
-            console.info(stderr);
-        })
-        process.on("message", console.info)
         console.info(`Performing prisma changes`)
+        const process = ChildProcess.exec(`npx prisma migrate deploy`, {})
         process.on("exit", (code, signal) => {
-            console.info(signal)
-            console.info(code)
-            console.info("Done");
-            resolve(0);
+            console.info("Done!");
+            resolve(0)
         })
         process.on("error", (error) => {
             console.error(error)
