e38444ba24a88ce78a52b407f13d7de1b6f52929
Gift Nnko <nnkogift@hotmail.com>
2023-07-20 12:54:54 +0300

    fix(build): Fixes
    - Removed packaging of app

--- postbuild.sh
+++ postbuild.sh
@@ -5,6 +5,6 @@ PKG_VERSION=$(node -p "require('./package.json').version")
 PKG_NAME=$(node -p "require('./package.json').name")
 echo "Building app"
 BUNDLE_NAME="$PKG_NAME-$PKG_VERSION.zip"
-zip "$BUNDLE_NAME" app package.json .env.example yarn.lock Dockerfile docker-compose.yml prisma
+bestzip "$BUNDLE_NAME" app package.json .env.example yarn.lock Dockerfile docker-compose.yml prisma
 mkdir "build"
 mv $BUNDLE_NAME build
